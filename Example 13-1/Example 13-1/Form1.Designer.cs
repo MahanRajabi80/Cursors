7f97e316bdf0ed5d82aa334389fb7e135a971f35
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 18:21:16 +0330

    Add files via upload

---
+++ Example 13-1/Example 13-1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Example_13_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbCursors = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbCursors
+            // 
+            this.cmbCursors.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCursors.FormattingEnabled = true;
+            this.cmbCursors.Location = new System.Drawing.Point(48, 88);
+            this.cmbCursors.Name = "cmbCursors";
+            this.cmbCursors.Size = new System.Drawing.Size(195, 21);
+            this.cmbCursors.TabIndex = 0;
+            this.cmbCursors.SelectedIndexChanged += new System.EventHandler(this.cmbCursors_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Cursors";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 220);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbCursors);
+            this.Name = "Form1";
+            this.Text = "Example 13-1 (Cursors)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbCursors;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Example 13-1/Example 13-1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_13_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //------------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            object[] cur = {Cursors.AppStarting,Cursors.Arrow,Cursors.Cross,Cursors.Default,
+                               Cursors.Hand,Cursors.Help,Cursors.HSplit,Cursors.IBeam,Cursors.No,
+                               Cursors.NoMove2D,Cursors.NoMoveHoriz,Cursors.NoMoveVert,
+                               Cursors.PanEast,Cursors.PanNE,Cursors.PanNorth,Cursors.PanNW,
+                               Cursors.PanSE,Cursors.PanSouth,Cursors.PanSW,Cursors.PanWest,
+                               Cursors.SizeAll,Cursors.SizeNESW,Cursors.SizeNS,Cursors.SizeNWSE,
+                               Cursors.SizeWE,Cursors.UpArrow,Cursors.VSplit,Cursors.WaitCursor
+                           };
+            cmbCursors.Items.AddRange((object[])cur);
+            cmbCursors.SelectedIndex = 0;
+            this.Cursor = Cursors.AppStarting;
+        }
+        //------------------------------------------------------------
+        private void cmbCursors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Cursor = (Cursor)cmbCursors.Items[cmbCursors.SelectedIndex];
+        }
+    }
+}
